a5ffcb9bac7a1b0fca5f76ea929a80511b382637
mauriceForJethro <maurice@jethroltd.com>
2025-08-26 07:55:50 +0100

    edit

--- src/main/java/com/booksystem/bookManagement/controller/FileUploadController.java
+++ src/main/java/com/booksystem/bookManagement/controller/FileUploadController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,6 +19,7 @@ import java.util.Base64;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
 public class FileUploadController {
 
     private final FileStorageService fileStorageService;
